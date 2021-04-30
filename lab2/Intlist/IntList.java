d1eb1f8bc5a5d5a948db54e96d8546674073c946
Tom <tomjing711@gmail.com>
2021-04-30 03:08:45 -0700

    v2 fix null

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null){
+            return null;
+        }
         IntList A_clone = A;
         while (A_clone.rest!= null){
             A_clone = A_clone.rest;
@@ -96,6 +99,9 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null){
+            return null;
+        }
         IntList res = new IntList(A.first, null);
         IntList res_clone = res;
         IntList A_clone = A.rest;
